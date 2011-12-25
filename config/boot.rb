dd733616e9ef81ccfc01f06979d35550f3f28b29
Darcy Laycock <sutto@sutto.net>
2011-12-25 22:23:09 +0800

    Work on converting the useful staging site

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,6 @@
 # Don't change this file!
 # Configure your app in config/environment.rb and config/environments/*.rb
-
+require 'thread'
 RAILS_ROOT = File.expand_path("#{File.dirname(__FILE__)}/..") unless defined?(RAILS_ROOT)
 
 module Rails
